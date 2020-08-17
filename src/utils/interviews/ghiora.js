6223322e97684804c223d0510ef3d3bec3660b77
neerajp99 <neerajp1999@gmail.com>
2020-08-17 20:43:42 +0530

    Add Ghiora interview

---
+++ src/utils/interviews/ghiora.js
@@ -0,0 +1,30 @@
+const imgBoy = require("../img/ghiora-art.png");
+const imgGirl = require("../img/ghiora.png");
+const interview = [
+  {
+    name: "Ghiora "
+  },
+  {
+    surname: "Aharoni"
+  },
+  {
+    about:
+      "Ghiora Aharoni founded his multi-disciplinary studio for art and design in New York City in 2004, and his work has been exhibited internationally in museums, institutions and galleries. A graduate of Yale University, Aharoni’s work is in the permanent collection of The Pompidou Center, The Vatican, The Kiran Nadar Museum of Art, The Morgan Library & Museum and The Beit Hatfutsot Museum in Tel Aviv—as well as numerous private collections in North America, Europe and India."
+  },
+  {
+    works: "https://www.ghiora-aharoni.com/"
+  },
+  {
+    images: [imgBoy, imgGirl]
+  },
+  {
+    questions: ["What is your view on computational creativity in art?"]
+  },
+  {
+    answers: [
+      "Your question about Computational and AI could not be more topical. I think the question of how one perceives the world—if what we see is 'real' or it is illusionary—remains an open and engaging question...so Computational/AI art is a quintessential and exciting way to explore  that question. The introduction of a “new” movement or form is often heralded as an innovation that will change the future (think video art in the 1960s). However, I tend to see it as something that expands or shifts our thinking about art, rather than changing it…emerging from and at the same time contributing to the creation of work. In our society, especially with the advent of the digital age, we have become fascinated/obsessed with the “new”—be it an invention, a work of art, a theory or a technology—and our desire to feel that what we are creating/experiencing something is a signpost for humanity that we are advancing. And while innovation and creativity are very much about creating something “new,” that process is codified in a chronology of before/after, what was/what is next. Though in the process of creation, we all reference/appropriate (either consciously or unconsciously) images, ideas, theories or concepts that we have encountered during our lives.  And that enormous collective—coupled with imagination and in some cases technology—forms the wellspring of creativity. As for the Art market, the work I create explores an idea or embodies an energy that I hope resonates with viewers…and a work of art can have many possible relationships and layers of engagement after it leaves the studio, so when it does, I think of it as having its own life."
+    ]
+  }
+];
+
+module.exports = interview;
